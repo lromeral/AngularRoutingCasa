402f019de87d0e8b94cdccc53740907597ea95ab
lromeral <luis.romeral@gmail.com>
2020-03-03 12:12:56 +0100

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PaisesComponent } from './paises/paises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule
